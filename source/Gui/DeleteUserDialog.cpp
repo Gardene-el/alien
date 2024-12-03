f108afd884ef3bc66ced3bb4d20f4516826444b5
Garden_eel <103943173+Gardene-el@users.noreply.github.com>
2024-12-03 19:35:04 +0800

    change delete user window into chinese

--- source/Gui/DeleteUserDialog.cpp
+++ source/Gui/DeleteUserDialog.cpp
@@ -10,26 +10,26 @@
 #include "GenericMessageDialog.h"
 
 DeleteUserDialog::DeleteUserDialog()
-    : AlienDialog("Delete user")
+    : AlienDialog("删除用户")
 {}
 
 void DeleteUserDialog::processIntern()
 {
     AlienImGui::Text(
-        "Warning: All the data of the user '" + *NetworkService::get().getLoggedInUserName()
-        + "' will be deleted on the server side.\nThese include the likes, the simulations and the account data.");
+        "警告: 该用户 '" + *NetworkService::get().getLoggedInUserName()
+        + "'的所有数据将在服务器中删除。\n这些包括收藏，模拟器和账户数据。");
     AlienImGui::Separator();
 
-    AlienImGui::InputText(AlienImGui::InputTextParameters().hint("Re-enter password").password(true).textWidth(0), _reenteredPassword);
+    AlienImGui::InputText(AlienImGui::InputTextParameters().hint("重新输入密码").password(true).textWidth(0), _reenteredPassword);
     AlienImGui::Separator();
 
     ImGui::BeginDisabled(_reenteredPassword.empty());
-    if (AlienImGui::Button("Delete")) {
+    if (AlienImGui::Button("删除")) {
         close();
         if (_reenteredPassword == *NetworkService::get().getPassword()) {
             onDelete();
         } else {
-            GenericMessageDialog::get().information("Error", "The password does not match.");
+            GenericMessageDialog::get().information("错误", "密码不匹配。");
         }
         _reenteredPassword.clear();
     }
@@ -37,7 +37,7 @@ void DeleteUserDialog::processIntern()
     ImGui::SetItemDefaultFocus();
 
     ImGui::SameLine();
-    if (AlienImGui::Button("Cancel")) {
+    if (AlienImGui::Button("取消")) {
         close();
         _reenteredPassword.clear();
     }
@@ -48,8 +48,8 @@ void DeleteUserDialog::onDelete()
     auto userName = *NetworkService::get().getLoggedInUserName();
     if (NetworkService::get().deleteUser()) {
         BrowserWindow::get().onRefresh();
-        GenericMessageDialog::get().information("Information", "The user '" + userName + "' has been deleted.\nYou are logged out.");
+        GenericMessageDialog::get().information("信息", "用户'" + userName + "'已经被删除\n你已经登出。");
     } else {
-        GenericMessageDialog::get().information("Error", "An error occurred on the server.");
+        GenericMessageDialog::get().information("错误", "一个错误发生在服务器。");
     }
 }
