70cd1cb33f309662e1e356ce5cd61d2d8510afe9
Garden_eel <103943173+Gardene-el@users.noreply.github.com>
2024-12-04 19:11:08 +0800

    change about dialog into chinese

--- source/Gui/AboutDialog.cpp
+++ source/Gui/AboutDialog.cpp
@@ -8,17 +8,17 @@
 #include "StyleRepository.h"
 
 AboutDialog::AboutDialog()
-    : AlienDialog("About")
+    : AlienDialog("关于ALIEN")
 {}
 
 void AboutDialog::processIntern()
 {
-    ImGui::Text("Artificial Life Environment, version %s\n\nis an open source project initiated and maintained by\nChristian Heinemann.", Const::ProgramVersion.c_str());
+    ImGui::Text("Artificial Life Environment, 版本号 %s\n\n是一个由 Christian Heinemann 开发和维护的开源项目。\n\nALIEN项目的中文翻译部分由Garden_eel维护。\n\nArtificial Life Environment, version %s\n\nis an open source project initiated and maintained by\nChristian Heinemann.\n\n The Chinese translation is maintained by Garden_eel.", Const::ProgramVersion.c_str(),Const::ProgramVersion.c_str());
 
     ImGui::Dummy({0, ImGui::GetContentRegionAvail().y - scale(50.0f)});
     AlienImGui::Separator();
 
-    if (AlienImGui::Button("OK")) {
+    if (AlienImGui::Button("确认")) {
         close();
     }
     ImGui::SetItemDefaultFocus();
