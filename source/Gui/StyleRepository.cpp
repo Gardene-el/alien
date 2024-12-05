0f9cd26b04a11f976e8d4ce3ef8541dd9906fc7b
Garden_eel <103943173+Gardene-el@users.noreply.github.com>
2024-12-05 15:40:36 +0800

    Revert "Add [font](https://github.com/atelier-anchor/smiley-sans) that support Chinese"
    This reverts commit cbc237ceaf4ab1925917620401c8e5cd23338598.

--- source/Gui/StyleRepository.cpp
+++ source/Gui/StyleRepository.cpp
@@ -3,19 +3,20 @@
 #include <stdexcept>
 
 #include <ImFileDialog.h>
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
 #include <imgui.h>
 #include <imgui_freetype.h>
 #include <implot.h>
 
-#include <GLFW/glfw3.h>  // Will drag system OpenGL headers
-
-#include "Fonts/AlienIconFont.h"
-#include "Fonts/Cousine-Regular.h"
 #include "Fonts/DroidSans.h"
 #include "Fonts/DroidSansBold.h"
+#include "Fonts/Cousine-Regular.h"
+#include "Fonts/AlienIconFont.h"
 #include "Fonts/FontAwesomeSolid.h"
 #include "Fonts/IconsFontAwesome5.h"
 #include "Fonts/Reef.h"
+
 #include "WindowController.h"
 
 void StyleRepository::setup()
@@ -31,13 +32,9 @@ void StyleRepository::setup()
     configMerge.FontBuilderFlags = ImGuiFreeTypeBuilderFlags_LightHinting;
 
     ImGuiIO& io = ImGui::GetIO();
-    const char* font_path = "resources\\SmileySans-Oblique.ttf";
-    io.Fonts->FontBuilderIO = ImGuiFreeType::GetBuilderForFreeType();
-    io.Fonts->FontBuilderFlags = ImGuiFreeTypeBuilderFlags_LightHinting;
 
     //default font (small with icons)
-    // io.Fonts->AddFontFromMemoryCompressedTTF(DroidSans_compressed_data, DroidSans_compressed_size, 16.0f * scaleFactor);
-    io.Fonts->AddFontFromFileTTF(font_path, 16.0f * scaleFactor, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
+    io.Fonts->AddFontFromMemoryCompressedTTF(DroidSans_compressed_data, DroidSans_compressed_size, 16.0f * scaleFactor);
     {
         static const ImWchar rangesIcons[] = {ICON_MIN_FA, ICON_MAX_FA, 0};
         io.Fonts->AddFontFromMemoryCompressedTTF(
@@ -45,27 +42,19 @@ void StyleRepository::setup()
     }
 
     //small bold font
-    // _smallBoldFont = io.Fonts->AddFontFromMemoryCompressedTTF(DroidSansBold_compressed_data, DroidSansBold_compressed_size, 16.0f * scaleFactor);
-    _smallBoldFont = io.Fonts->AddFontFromFileTTF(font_path, 16.0f * scaleFactor, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
+    _smallBoldFont = io.Fonts->AddFontFromMemoryCompressedTTF(DroidSansBold_compressed_data, DroidSansBold_compressed_size, 16.0f * scaleFactor);
 
     //medium bold font
-    // _mediumBoldFont = io.Fonts->AddFontFromMemoryCompressedTTF(DroidSansBold_compressed_data, DroidSansBold_compressed_size, 24.0f * scaleFactor);
-    _mediumBoldFont = io.Fonts->AddFontFromFileTTF(font_path, 24.0f * scaleFactor, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
-
+    _mediumBoldFont = io.Fonts->AddFontFromMemoryCompressedTTF(DroidSansBold_compressed_data, DroidSansBold_compressed_size, 24.0f * scaleFactor);
 
     //medium font
-    // _mediumFont = io.Fonts->AddFontFromMemoryCompressedTTF(DroidSans_compressed_data, DroidSans_compressed_size, 24.0f * scaleFactor);
-    _mediumFont = io.Fonts->AddFontFromFileTTF(font_path, 24.0f * scaleFactor, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
-
+    _mediumFont = io.Fonts->AddFontFromMemoryCompressedTTF(DroidSans_compressed_data, DroidSans_compressed_size, 24.0f * scaleFactor);
 
     //large font
-    // _largeFont = io.Fonts->AddFontFromMemoryCompressedTTF(DroidSans_compressed_data, DroidSans_compressed_size, 48.0f * scaleFactor);
-    _largeFont = io.Fonts->AddFontFromFileTTF(font_path, 48.0f * scaleFactor, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
-
+    _largeFont = io.Fonts->AddFontFromMemoryCompressedTTF(DroidSans_compressed_data, DroidSans_compressed_size, 48.0f * scaleFactor);
 
     //icon font
-    // _iconFont = io.Fonts->AddFontFromMemoryCompressedTTF(AlienIconFont_compressed_data, AlienIconFont_compressed_size, 24.0f * scaleFactor);
-    _iconFont = io.Fonts->AddFontFromFileTTF(font_path, 24.0f * scaleFactor, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
+    _iconFont = io.Fonts->AddFontFromMemoryCompressedTTF(AlienIconFont_compressed_data, AlienIconFont_compressed_size, 24.0f * scaleFactor);
     {
         static const ImWchar rangesIcons[] = {ICON_MIN_FA, ICON_MAX_FA, 0};
         io.Fonts->AddFontFromMemoryCompressedTTF(
@@ -74,17 +63,13 @@ void StyleRepository::setup()
     }
 
     //monospace medium font
-    // _monospaceMediumFont = io.Fonts->AddFontFromMemoryCompressedTTF(Cousine_Regular_compressed_data, Cousine_Regular_compressed_size, 14.0f * scaleFactor);
-    _monospaceMediumFont = io.Fonts->AddFontFromFileTTF(font_path, 14.0f * scaleFactor, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
+    _monospaceMediumFont = io.Fonts->AddFontFromMemoryCompressedTTF(Cousine_Regular_compressed_data, Cousine_Regular_compressed_size, 14.0f * scaleFactor);
 
     //monospace large font
-    // _monospaceLargeFont = io.Fonts->AddFontFromMemoryCompressedTTF(Cousine_Regular_compressed_data, Cousine_Regular_compressed_size, 128.0f * scaleFactor);
-    _monospaceLargeFont = io.Fonts->AddFontFromFileTTF(font_path, 128.0f * scaleFactor, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
-    // _reefMediumFont = io.Fonts->AddFontFromMemoryCompressedTTF(Reef_compressed_data, Reef_compressed_size, 24.0f * scaleFactor);
-    _reefMediumFont = io.Fonts->AddFontFromFileTTF(font_path, 24.0f * scaleFactor, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
-    // _reefLargeFont = io.Fonts->AddFontFromMemoryCompressedTTF(Reef_compressed_data, Reef_compressed_size, 64.0f * scaleFactor);
-    _reefLargeFont = io.Fonts->AddFontFromFileTTF(font_path, 64.0f * scaleFactor, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
-    io.Fonts->Build();
+    _monospaceLargeFont = io.Fonts->AddFontFromMemoryCompressedTTF(Cousine_Regular_compressed_data, Cousine_Regular_compressed_size, 128.0f * scaleFactor);
+
+    _reefMediumFont = io.Fonts->AddFontFromMemoryCompressedTTF(Reef_compressed_data, Reef_compressed_size, 24.0f * scaleFactor);
+    _reefLargeFont = io.Fonts->AddFontFromMemoryCompressedTTF(Reef_compressed_data, Reef_compressed_size, 64.0f * scaleFactor);
 }
 
 ImFont* StyleRepository::getIconFont() const
